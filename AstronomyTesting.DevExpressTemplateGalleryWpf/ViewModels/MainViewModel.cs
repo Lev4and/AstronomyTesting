6eab62fd0a4d557f78c838ba76f25214b5dd27c4
Lev4and <54065747+Lev4and@users.noreply.github.com>
2021-03-03 18:49:01 +0500

    Установка библиотек. Внедрение MaterialDesignThemes и MaterialDesignColors в проект. Написан интерфейс первого окна. Реализована функция закрытия приложения по нажатию кнопки.

--- AstronomyTesting.DevExpressTemplateGalleryWpf/ViewModels/MainViewModel.cs
+++ AstronomyTesting.DevExpressTemplateGalleryWpf/ViewModels/MainViewModel.cs
@@ -1,9 +1,24 @@
 ﻿using DevExpress.Mvvm;
-using System;
+using DevExpress.Mvvm.POCO;
+using System.Windows;
 
 namespace AstronomyTesting.DevExpressTemplateGalleryWpf.ViewModels
 {
     public class MainViewModel : ViewModelBase
     {
+        protected MainViewModel()
+        {
+
+        }
+
+        public static MainViewModel Create()
+        {
+            return ViewModelSource.Create(() => new MainViewModel());
+        }
+
+        public void Exit()
+        {
+            Application.Current.Shutdown();
+        }
     }
 }
